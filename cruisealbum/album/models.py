7549fe36ebf45d030b74989f1d7c319c8343f285
Tatenda  Katema <primewebwizards@gmail.com>
2024-06-26 20:01:34 +0200

    basic structure

--- cruisealbum/album/models.py
+++ cruisealbum/album/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class City(models.Model):
+    """the class to store details about the city"""
+    name = models.CharField(max_length=40)
+    country = models.CharField(max_length=40)
+    bio = models.CharField
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField(max_length=200, blank=True)
+    no_of_likes = models.IntegerField(default=0)
+    no_of_downloads = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.city.name
--- cruisealbum/album/views.py
+++ cruisealbum/album/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import City, Post
 
 # Create your views here.
 def homePage(request):
